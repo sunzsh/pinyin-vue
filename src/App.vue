ffae4dfb0868b4040194ae76623a8f7c4b51569a
sunzsh <cn.xiaoshan@gmail.com>
2022-12-10 18:31:49 +0800

    增加错题本

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
 <template>
   <div id="app">
-    <div class="main" @click="next">{{ current }}</div>
-    <div class="desc">剩余：{{ data.length }}</div>
+    <div class="main" @click="next" v-longpress="longpress">{{ currentData.current }}</div>
+    <div class="desc">{{mode == 'wrongs' ? '错题本复习，' : '题库练习，'}}剩余：{{ currentData.data.length }}</div>
+
+    <div class="menu-wrapper" v-show="showingMenu" @click="showingMenu=false">
+      <div class="flex-spacer"></div>
+      <div class="menu">
+        <div class="menu-item" v-show="currentData.prev && currentData.prev !== currentData.current" @click="goPrev">上一个</div>
+        <div class="menu-item" @click="addWrongs" v-show="mode !== 'wrongs'">加入错题本</div>
+        <div class="menu-item" @click="openWrongs" v-show="wrongs.data.length > 0 && mode !== 'wrongs'">进入错题本 {{ `[${wrongs.data.length}个]` }}</div>
+        <div class="menu-item" @click="removeCurrentWrong" v-show="mode == 'wrongs'">记住了，移出错题本：<span style="font-family: 'pinyin'">{{currentData.current}}</span></div>
+        <div class="menu-item" @click="clearWrongs" v-show="mode == 'wrongs'" style="color: #F56C6C;">清空错题本</div>
+        <div class="menu-item" @click="exitWrongs" v-show="mode == 'wrongs'">退出错题本</div>
+      </div>
+      <div class="flex-spacer"></div>
+    </div>
   </div>
 </template>
 
@@ -13,22 +26,89 @@ import data1 from '@/assets/py-data1.js'
 export default {
   data () {
     return {
-      data: [...data1],
-      current: [[]]
+      currentData: { data: [] },
+      normal: {
+        current: '',
+        prev: '',
+        bakCurrent: '',
+        data: [...data1],
+      },
+      wrongs: {
+        current: '',
+        prev: '',
+        bakCurrent: '',
+        data: JSON.parse(localStorage.getItem('pinyin-wrongs')) || [],
+      },
+      showingMenu: false
+    }
+  },
+  computed: {
+    mode () {
+      return this.currentData === this.normal ? 'normal' : 'wrongs'
     }
   },
   mounted () {
-    // console.log(this.data);
-    this.next()
+    this.currentData = this.normal
+    this.next(true)
   },
   methods: {
-    next () {
-      if (this.data.length === 0) {
+    next (ignoreMenu) {
+      if (!ignoreMenu && this.showingMenu) {
+        return
+      }
+      if (this.currentData.data.length === 0) {
         alert('恭喜你 练完了，刷新重来！')
         return;
       }
+      this.currentData.prev = this.currentData.current
+      if (this.currentData.bakCurrent) {
+        this.currentData.current = this.currentData.bakCurrent
+        this.currentData.bakCurrent = ''
+        return
+      }
       // 从 this.data 中随机提取一个汉字并从 this.data 中删除
-      this.current = (pinyin(this.data.splice(Math.floor(Math.random() * this.data.length), 1)[0])[0]||{})[0]
+      let next = this.currentData.data.splice(Math.floor(Math.random() * this.currentData.data.length), 1)[0];
+      // 正则判断：next如果是汉字
+      if (/^[\u4e00-\u9fa5]+$/.test(next)) {
+        next = (pinyin(next)[0]||{})[0] // 转拼音
+      }
+      this.currentData.current = next
+    },
+    goPrev() {
+      this.currentData.bakCurrent = this.currentData.current
+      this.currentData.current = this.currentData.prev
+    },
+    longpress() {
+      this.showingMenu = true
+    },
+    addWrongs() {
+      if (this.wrongs.data.indexOf(this.currentData.current) !== -1) {
+        return
+      }
+      this.wrongs.data.push(this.currentData.current)
+      localStorage.setItem("pinyin-wrongs", JSON.stringify(this.wrongs.data))
+    },
+    openWrongs() {
+      this.currentData = this.wrongs
+      this.next(true)
+    },
+    removeCurrentWrong() {
+      this.wrongs.data.splice(this.wrongs.data.indexOf(this.currentData.current), 1)
+      localStorage.setItem("pinyin-wrongs", JSON.stringify(this.wrongs.data))
+    },
+    clearWrongs() {
+      if (window.confirm('确认要清空错题本吗？')) {
+        this.wrongs.data = []
+        this.wrongs.current = ''
+        this.wrongs.prev = ''
+        this.wrongs.bakCurrent = ''
+        localStorage.setItem("pinyin-wrongs", JSON.stringify(this.wrongs.data))
+        this.exitWrongs()
+      }
+    },
+    exitWrongs() {
+      this.currentData = this.normal
+      this.next(true)
     }
   }
 }
@@ -56,5 +136,34 @@ export default {
   user-select: none;
   cursor: default;
   color: rgb(157, 157, 157);
+}
+.menu-wrapper {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 9;
+  display: flex;
+  flex-direction: column;
+}
+.menu-wrapper .menu .menu-item {
+  margin: 0 auto;
+  display: block;
+  width: 20%;
+  min-width: 150px;
+  text-align: center;
+  background-color: #fff;
+  padding: 20px 0;
+  border-radius: 10px;
+  cursor: pointer;
+  user-select: none;
+}
+.menu-wrapper .menu .menu-item+ .menu-item {
+  margin-top: 25px;
+}
+.flex-spacer {
+  flex: 1;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
+import LongPress from './longpress.js'
+Vue.use(LongPress, {time: 500})
 
 Vue.config.productionTip = false
 
