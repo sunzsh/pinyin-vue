f8ec06185e2df3e093d8662d186a95dcff2115db
sunzsh <cn.xiaoshan@gmail.com>
2022-12-10 16:54:27 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,60 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="main" @click="next">{{ current }}</div>
+    <div class="desc">剩余：{{ data.length }}</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import pinyin from 'pinyin'
+
+import data1 from '@/assets/py-data1.js'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      data: [...data1],
+      current: [[]]
+    }
+  },
+  mounted () {
+    // console.log(this.data);
+    this.next()
+  },
+  methods: {
+    next () {
+      if (this.data.length === 0) {
+        alert('恭喜你 练完了，刷新重来！')
+        return;
+      }
+      // 从 this.data 中随机提取一个汉字并从 this.data 中删除
+      this.current = (pinyin(this.data.splice(Math.floor(Math.random() * this.data.length), 1)[0])[0]||{})[0]
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  overflow: hidden;
+  height: 100vh;
+}
+.main {
+  font-family: 'pinyin';
+  font-size: calc(20vw * 1.2);
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: auto 0;
+  height: 90vh;
+  line-height: 90vh;
+  user-select: none;
+  cursor: default;
+
+}
+.desc {
+  padding-top: 12px;
+  text-align: center;
+  user-select: none;
+  cursor: default;
+  color: rgb(157, 157, 157);
 }
 </style>
